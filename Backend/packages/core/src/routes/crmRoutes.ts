1fa93daa21b50c37347cf28ea608ce07403ee17c
DCLOC\aleksandar.ivanov <aleksandar.ivanov@smartify-it.de>
2018-10-18 13:18:00 +0200

    crm routes controller and model

--- Backend/packages/core/src/routes/crmRoutes.ts
+++ Backend/packages/core/src/routes/crmRoutes.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
+import { UserController } from "./user/user.controller";
 
 export class Routes {
+
+  public userController: UserController = new UserController();
+
   public routes(app): void {
+
     app.route('/')
       .get((req: Request, res: Response) => {
         res.status(200).send({
           message: 'GET request successfulll!!!!'
         })
       })
+
+    // User 
+    app.route('/user')
+      .get(this.userController.getUsers)
+      .post(this.userController.addNewUser)
+
+    // user detail
+    app.route('/user/:userId')
+      .put(this.userController.updateUser)
+
   }
 }
