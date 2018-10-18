7078015c9bd380bdda6d3c061cd23248eda1dbf2
DCLOC\aleksandar.ivanov <aleksandar.ivanov@smartify-it.de>
2018-10-18 14:53:44 +0200

    change http to https

--- Backend/packages/core/src/server/server.ts
+++ Backend/packages/core/src/server/server.ts
@@ -1,6 +1,14 @@
 import app from "../app/app";
 import { ServerConfig } from "./server.config";
+import * as https from 'https';
+import * as fs from 'fs';
 
-app.listen(ServerConfig.port, () => {
-  console.log('Express server listening on port ' + ServerConfig.port);
-})
+const httpsOptions = {
+  key: fs.readFileSync('./packages/core/src/server/certificates/key.pem'),
+  cert: fs.readFileSync('./packages/core/src/server/certificates/cert.pem')
+}
+
+https.createServer(httpsOptions, app)
+  .listen(ServerConfig.port, () => {
+    console.log('Express server listening on port ' + ServerConfig.port);
+  })
