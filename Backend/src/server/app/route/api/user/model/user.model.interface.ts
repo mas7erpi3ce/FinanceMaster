0fff7d84eb0da832583e23ddc8bb8d929934acb8
mas7erpi3ce <aleksandar.ivanov@smartify-it.de>
2019-02-20 19:55:38 +0100

    new structure

---
+++ Backend/src/server/app/route/api/user/model/user.model.interface.ts
@@ -0,0 +1,8 @@
+
+import { User } from "../user.interface";
+import { Document } from 'mongoose';
+
+export interface UserModel extends User, Document {
+  createdAt: Date;
+  comparePassword(password: string): boolean;
+}
