f54e1e47c7c0456b07e431bef3a3e0da9d6b86b9
mas7erpi3ce <aleksandar.ivanov@smartify-it.de>
2019-02-25 13:52:26 +0100

    mobile app first commit

---
+++ MobileApp/src/app/item/item.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+
+import { Item } from "./item";
+
+@Injectable({
+    providedIn: "root"
+})
+export class ItemService {
+    private items = new Array<Item>(
+        { id: 1, name: "Ter Stegen", role: "Goalkeeper" },
+        { id: 3, name: "Piqué", role: "Defender" },
+        { id: 4, name: "I. Rakitic", role: "Midfielder" },
+        { id: 5, name: "Sergio", role: "Midfielder" },
+        { id: 6, name: "Denis Suárez", role: "Midfielder" },
+    );
+
+    getItems(): Array<Item> {
+        return this.items;
+    }
+
+    getItem(id: number): Item {
+        return this.items.filter((item) => item.id === id)[0];
+    }
+}
