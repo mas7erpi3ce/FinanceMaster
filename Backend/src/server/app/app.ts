94d15350f4f1087c8f50a9bd2cecaffee666b584
mas7erpi3ce <aleksandar.ivanov@smartify-it.de>
2019-02-27 08:48:48 +0100

    change max upload size

--- Backend/src/server/app/app.ts
+++ Backend/src/server/app/app.ts
@@ -27,7 +27,7 @@ class App {
     // support application/json type post data
     this.app.use(bodyParser.json());
     // support application/x-www-form-urlencoded post data
-    this.app.use(bodyParser.urlencoded({ extended: false, limit: "5mb"}));
+    this.app.use(bodyParser.urlencoded({ extended: true, limit: "10mb"}));
     // Cross-Origin Resource Sharing (CORS)
     this.app.use(cors())
   }
